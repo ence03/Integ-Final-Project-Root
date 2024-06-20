50cb8b4e66cb4eb26fe39cee2bbdf6b09032777b
ence03 <justclarex.03@gmail.com>
2024-06-20 20:10:55 +0800

    components added (Naivgation, Login screen)

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,7 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./src/screen/LoginScreen";
 
 const Stack = createStackNavigator();
 
@@ -9,16 +8,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen /> //Add your screens here
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: "#fff",
-  },
-});
---
+++ mobile-app/src/screen/LoginScreen.js
@@ -0,0 +1,114 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import Logo from "../../assets/image/logo.png";
+import Icon from "react-native-vector-icons/Ionicons";
+import CheckBox from "expo-checkbox";
+
+const LoginScreen = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.icon}>
+        <Icon name="help-circle-outline" size={45} color="black" />
+      </TouchableOpacity>
+      <View>
+        <Image source={Logo} style={styles.logo} resizeMode="contain" />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Username" style={styles.input} />
+        <View style={styles.passwordContainer}>
+          <TextInput
+            placeholder="Password"
+            style={styles.input}
+            secureTextEntry={!passwordVisible}
+          />
+          <TouchableOpacity
+            style={styles.eye}
+            onPress={() => setPasswordVisible(!passwordVisible)}
+          >
+            <Icon name="eye-outline" size={25} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <View style={styles.checkbox}>
+        <CheckBox
+          value={isChecked}
+          onValueChange={setIsChecked}
+          color={isChecked ? "#74aef5" : undefined}
+        />
+        <Text>Remember me</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 300,
+  },
+  container: {
+    flex: 1,
+    maxWidth: "100%",
+    height: "100%",
+    padding: 5,
+    justifyContent: "top",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  passwordContainer: {
+    width: "100%",
+  },
+  eye: {
+    position: "absolute",
+    right: 0,
+  },
+  inputContainer: {
+    width: "90%",
+    marginBottom: 10,
+  },
+  input: {
+    borderColor: "black",
+    borderBottomWidth: 1,
+    padding: 5,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#74aef5",
+    padding: 5,
+    width: "83%",
+    marginBottom: 10,
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "semibold",
+    fontSize: 20,
+  },
+  icon: {
+    width: "100%",
+    alignItems: "flex-end",
+    marginRight: 10,
+  },
+  checkbox: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "90%",
+  },
+});
+
+export default LoginScreen;
