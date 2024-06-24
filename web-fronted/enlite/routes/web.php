0fbc900aa5bf8ea18d50d9544833371cd22b88fa
carlodominic <suaybaguio.carlodominic@gmail.com>
2024-06-25 01:56:33 +0800

    adding a logo pic and changing the route for login page

--- web-fronted/enlite/routes/web.php
+++ web-fronted/enlite/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
