2bb7eb11c2bf3596fc59b4de33a1ff74c790b29e
RickyboyDiez <125734104+RickyboyDiez@users.noreply.github.com>
2024-06-25 14:49:31 +0800

    added routes for courseinstructor controller

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\StudentDataController;
 use App\Http\Controllers\AdminEnrollmentController;
+use App\Http\Controllers\CourseInstructor;
 
 
 Route::get('/', function () {
@@ -36,3 +37,10 @@ Route::post('enrollments/store', [AdminEnrollmentController::class, 'store'])->n
 Route::delete('enrollments/{id}', [AdminEnrollmentController::class, 'destroy'])->name('enrollments.destroy');
 Route::get('/course/instructors', [AdminEnrollmentController::class, 'fetchInstructorsByCourse'])->name('course.instructors');
 Route::get('/course/instructor/by/course/instructor', [AdminEnrollmentController::class, 'fetchCourseInstructorByCourseInstructor'])->name('course.instructor.by.course.instructor');
+
+Route::get('/instructors', [CourseInstructor::class, 'index'])->name('instructors.index');
+Route::get('/instructors/{id}/edit', [CourseInstructor::class, 'edit'])->name('instructors.edit');
+Route::put('/instructors/{id}', [CourseInstructor::class, 'update'])->name('instructors.update');
+Route::get('/instructors/{id}/courses', [CourseInstructor::class, 'showCourses'])->name('instructors.courses');
+Route::post('/instructors/{id}/courses/{courseId}/add', [CourseInstructor::class, 'addCourse'])->name('instructors.courses.add');
+Route::put('/instructors/{id}/courses/{courseId}/drop', [CourseInstructor::class, 'dropCourse'])->name('instructors.courses.drop');
