d44087326853e6e7ecdacd27f59424f3439c1432
carlodominic <suaybaguio.carlodominic@gmail.com>
2024-06-25 23:30:39 +0800

    modifying web

--- web-fronted/enlite/routes/web.php
+++ web-fronted/enlite/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\dashboardTeacher;
+use App\Http\Controllers\dashboardStudent;
 
 Route::get('/', function () {
     return view('login');
